14a76bcb7e8d4849b44c17e65ef3d1925a6c66c2
JonhXQ <LopezJuarezJese.123@hotmail.com>
2024-03-18 14:42:16 -0600

    CURSO 1

--- CURSO-1/Aula-1-4/1_ProyectoInicial/app.js
+++ CURSO-1/Aula-1-4/1_ProyectoInicial/app.js
@@ -1,16 +1,14 @@
 alert('Bienvenido al juego del número secreto');
 
 /* Variables */
-let numeroSecretoMaximo = 20;
-let intentosMaximos = 5;
-let intentos = 1;
+let numeroSecreto = 2;
 let numeroUsuario = 0;
-let numeroSecreto = parseInt(Math.random() * numeroSecretoMaximo) + 1;
+let intentos = 1;
+let palabraIntentos = 'vez';
 
 while (numeroUsuario != numeroSecreto) {
-    console.log('Valor del número secreto:', numeroSecreto);
     // Agrega un console.log para verificar el valor de "numeroUsuario" después de la entrada del usuario
-    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroSecretoMaximo} por favor:`));
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
     console.log('Valor de intento:', numeroUsuario);
 
     // Agrega un console.log para verificar la comparación entre "numeroUsuario" y "numeroSecreto"
@@ -19,7 +17,10 @@ while (numeroUsuario != numeroSecreto) {
     /* Este código realiza la comparación */
     if (numeroUsuario == numeroSecreto) {
         /* Acertamos, fue verdadera la condición */
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraIntentos}`);
+
+        // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
+        console.log('Valor del número secreto:', numeroSecreto);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
@@ -27,11 +28,7 @@ while (numeroUsuario != numeroSecreto) {
             alert('El número secreto es mayor');
         }
         /* Incrementamos el operador cuando la persona no acierta */
-        intentos++;
-
-        if (intentos > intentosMaximos) {
-            alert(`Llegaste al número máximo de ${intentosMaximos} intentos. El número secreto era el ${numeroSecreto}`);
-            break;
-        }
+        intentos += 1;
+        palabraIntentos = 'veces';
     }
 }
